557e4d5bdebb3164c63330d45a47514f6639a3b5
sadhanavadrevu <sadhana.vadrevu@gmail.com>
2020-01-13 15:57:12 -0800

    Add files via upload

--- Project1/logic_error.cpp
+++ Project1/logic_error.cpp
@@ -1,39 +1,41 @@
 //
-//  compile_error.cpp
+//  logic_error.cpp
 //  Project 1
-//  change code so that it is unable to produce poll results
+//  report poll results inaccurately
 
 //  Created by Sadhana Vadrevu on 10/5/18.
 //  Copyright © 2018 Sadhana Vadrevu. All rights reserved.
 //
 
 #include <iostream>
-//did not include namespace std
+using namespace std;
 
-int main  //no parantheses next to main function
+int main()
 {
     int numberSurveyed;
     int forNewsom;
-    int forCox  //omitted a semicolon
+    int forCox;
     
     cout << "How many registered voters were surveyed? ";
     cin >> numberSurveyed;
     cout << "How many of them say they will vote for Newsom? ";
     cin >> forNewsom;
     cout << "How many of them say they will vote for Cox? ";
-    cin >> forCox
+    cin >> forCox;
     
     double pctNewsom = 100.0 * forNewsom / numberSurveyed;
     double pctCox = 100.0 * forCox / numberSurveyed;
     
+    cout.setf(ios::fixed);
     cout.precision(1);
     
-    cout endl;
-    cout << pctNewsom << "% say they will vote for Newsom." << endl;
+    cout << endl;
+    cout << forNewsom << "% say they will vote for Newsom." << endl; //shows number of voters for Newsom rather than the accurate percentage of voters
     cout << pctCox << "% say they will vote for Cox." << endl;
     
-    if (forNewsom > forCox)
+    if (forNewsom < forCox) //switched inequality so that result would be wrong, smaller number wins
         cout << "Newsom is predicted to win the election." << endl;
     else
         cout << "Cox is predicted to win the election." << endl;
 }
+
--- Project1/original.cpp
+++ Project1/original.cpp
@@ -1,34 +1,29 @@
-//
-//  compile_error.cpp
-//  Project 1
-//  change code so that it is unable to produce poll results
-
-//  Created by Sadhana Vadrevu on 10/5/18.
-//  Copyright © 2018 Sadhana Vadrevu. All rights reserved.
-//
+// Code for Project 1
+// Report poll results
 
 #include <iostream>
-//did not include namespace std
+using namespace std;   // pp. 38-39 in Savitch 6/e explains this line
 
-int main  //no parantheses next to main function
+int main()
 {
     int numberSurveyed;
     int forNewsom;
-    int forCox  //omitted a semicolon
+    int forCox;
     
     cout << "How many registered voters were surveyed? ";
     cin >> numberSurveyed;
     cout << "How many of them say they will vote for Newsom? ";
     cin >> forNewsom;
     cout << "How many of them say they will vote for Cox? ";
-    cin >> forCox
+    cin >> forCox;
     
     double pctNewsom = 100.0 * forNewsom / numberSurveyed;
     double pctCox = 100.0 * forCox / numberSurveyed;
     
+    cout.setf(ios::fixed);       // see pp. 32-33 in Savitch 6/e
     cout.precision(1);
     
-    cout endl;
+    cout << endl;
     cout << pctNewsom << "% say they will vote for Newsom." << endl;
     cout << pctCox << "% say they will vote for Cox." << endl;
     
